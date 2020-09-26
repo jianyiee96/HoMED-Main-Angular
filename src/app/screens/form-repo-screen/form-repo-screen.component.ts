4eb0b28255081043ad583281287d4a389e39d8b1
Trishpal <e0310925@u.nus.edu>
2020-09-26 19:26:13 +0800

    Form repo working

--- src/app/screens/form-repo-screen/form-repo-screen.component.ts
+++ src/app/screens/form-repo-screen/form-repo-screen.component.ts
@@ -4,6 +4,7 @@ import {BreadcrumbService} from '../../services/breadcrum.service';
 import { MessageService } from 'primeng/api';
 import { FormService } from 'src/app/services/form/form.service';
 import { FormTemplate } from 'src/app/classes/formtemplate/formtemplate';
+import { SessionService } from 'src/app/services/session/session.service'
 
 @Component({
   selector: 'app-form-repo-screen',
@@ -13,11 +14,17 @@ import { FormTemplate } from 'src/app/classes/formtemplate/formtemplate';
 })
 export class FormRepoScreenComponent implements OnInit {
   
+  
   formTemplates: FormTemplate[]
   selectedTemplate: FormTemplate
   selected: boolean
+  
+  
 
-  constructor(private breadcrumbService: BreadcrumbService, private formService: FormService) { 
+
+  constructor(private breadcrumbService: BreadcrumbService, private formService: FormService, private sessionService: SessionService,
+              private service: MessageService
+  ) { 
     this.breadcrumbService.setItems([
       {label: 'eForm Management'},
       {label: 'General eForms'},
@@ -32,6 +39,7 @@ export class FormRepoScreenComponent implements OnInit {
       response => {
         this.formTemplates = response.formTemplates
         this.selected = false
+      
       },
       error => {
 				console.log(error.substring(32))
@@ -40,12 +48,30 @@ export class FormRepoScreenComponent implements OnInit {
     
   }
 
-  onRowSelect(event) {
-    this.selected = true
+
+  createFormInstance() {
+    this.formService.createFormInstance(this.sessionService.getCurrentServiceman().servicemanId, this.selectedTemplate.formTemplateId).subscribe(
+      response => {  
+        console.log("Success")
+        this.service.add({ key: 'tst', severity: 'success', summary: '', detail: 'Form Instance Created Successfully' });
+      },
+      error => {
+				console.log(error.substring(32))
+			}
+    );
   }
+
+  onRowSelect(event) { 
+    this.selected = true
+    this.selectedTemplate.datePublished= this.parseDate(this.selectedTemplate.datePublished).substring(0,10)
+  } 
 
   onRowUnselect(event) {
     this.selected = false
+  }
+
+  parseDate(date: any) {
+    return date.toString().replace('[UTC]', '');
   }
 
 }
